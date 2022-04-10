a286425095c952dad594a688e6aca6274577a45a
danbi7184 <danbi7184@gmail.com>
2022-04-10 15:55:20 +0900

    21st file

--- node_modules/payload/dist/admin/components/views/collections/List/formatFields.d.ts
+++
@@ -1,4 +0,0 @@
-import { SanitizedCollectionConfig } from '../../../../../collections/config/types';
-import { Field } from '../../../../../fields/config/types';
-declare const formatFields: (config: SanitizedCollectionConfig) => Field[];
-export default formatFields;
