67e68352a121a839e766fac00673a4b3bf40e9c6
danbi7184 <danbi7184@gmail.com>
2022-04-16 22:22:49 +0900

    Add btrainSttus & lastTrain

--- MMM-SeoulSubway.js
+++ MMM-SeoulSubway.js
@@ -41,12 +41,16 @@ Module.register("MMM-SeoulSubway", {
     var RowArr = new Array();
     var updnLineArr = new Array();
     var trainLineNmArr = new Array();
+    var btrainSttusArr = new Array();
+    var lastTrainArr = new Array();
 
     for(var i=0; i<subway.length; i++) {
       if (subway[i].trainLineNm._text.includes(this.config.direction)) {
         RowArr[i] = 'row' + i;
         updnLineArr[i] = 'updnLine' + i;
         trainLineNmArr[i] = 'trainLineNm' + i;
+        btrainSttusArr[i] = 'btrainSttus' + i;
+        lastTrainArr[i] = 'lastTrainArr' + i;
 
         RowArr[i] = document.createElement("tr");
         RowArr[i].className = "title bright";
@@ -71,12 +75,19 @@ Module.register("MMM-SeoulSubway", {
           RowArr[i].appendChild(trainLineNmArr[i]);
         }
 
+        // 열차 종류 (급행 / ITX)
+        if (subway[i].trainLineNm._text.includes("급행"))
+        {
+          btrainSttusArr[i] = document.createElement("td");
+          btrainSttusArr[i].innerHTML = "(급행)";
+        }
 
-        // 전역 진입, 전역 도착 혹은 몇분 후 도착
-        //var arvlMsg2 = document.createElement("td");
-        // arvlMsg2.className = "arriving";
-        // arvlMsg2.innerHTML = subway.arvlMsg2._text;
-        // tableRow.appendChild(arvlMsg2);
+        // 막차 표시
+        if (subway[i].trainLineNm._text.includes("막차"))
+        {
+          lastTrainArr[i] = document.createElement("td");
+          lastTrainArr[i].innerHTML = "(막차)";
+        }
       }
     }
     wrapper.appendChild(subwayTable);
